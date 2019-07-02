925e9b007472e4d0b6162fe3f279018e84a0ade0
Tiemo Hunger <tiemo.hunger@sidenis.com>
2019-07-02 13:13:31 +0200

    fixes

--- src/server.js
+++ src/server.js
@@ -60,15 +60,16 @@ const resolvers = {
     clearingRules:(_,{})=>{
       return ["RULE1"];
     }
-    
-    
 	}
 };
 
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({cors:{
+  origin: '*',            // <- allow request from all domains
+  credentials: true    
+} ,typeDefs, resolvers });
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
